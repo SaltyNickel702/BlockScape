1ebe37bb393627447523452d3859b92e93d2d0c2
Niko Wolf <niko.m.wolf@gmail.com>
2025-03-28 17:54:39 -0600

    Edited some stuff + added glm library

--- src/Game.h
+++ src/Game.h
@@ -5,6 +5,9 @@
 #include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 #include <thread>
 #include <functional>
 #include <vector>
--- src/world.h
+++ src/world.h
@@ -2,6 +2,7 @@
 #include <vector>
 #include <map>
 
+
 #include "Block.h"
 #include "Chunk.h"
 #include "Model.h"
@@ -13,15 +14,16 @@ using namespace std;
 
 namespace World { //world data
     extern map<int, map<int,Chunk>> chunks;
-    const vector<float*> getChunkMesh (int cx, int cy); //two float arrays, one for vertices, one for indices
-    static vector<float*> joinChunkMeshes (int x, int y, Chunk chunks[]);
+    const Model getChunkMesh (int cx, int cy); //two float arrays, one for vertices, one for indices
+    static Model joinChunkMeshes (Chunk chunks[]);
 
     extern map<int,Block> blockTypes;
 
     extern vector<Model*> models;
     
+    Chunk* getChunk (int x, int y);
+    int* getBlock (int x, int y, int z);
     void setBlock (int x, int y, int z, int block);
-    int getBlock (int x, int y, int z);
     
     namespace Camera {
         extern float pos[3];

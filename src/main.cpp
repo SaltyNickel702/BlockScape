606ae00dfec85a8220fbc124e9ac31d870bec129
Niko Wolf <niko.m.wolf@gmail.com>
2025-03-28 16:35:47 -0600

    Added draw function to model class

--- src/main.cpp
+++ src/main.cpp
@@ -2,5 +2,8 @@
 #include "Game.h" //includes all needed includes
 
 int main () {
-    Game::init(800,800);
+    // Game::init(800,800);
+    thread render(Game::init,800,800);
+
+    render.join();
 }

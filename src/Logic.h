bd6a7fc8fdc00105b26659032c2c3e5805244857
Niko Wolf <niko.m.wolf@gmail.com>
2025-03-31 20:32:45 -0600

    Update Logic.h

--- src/Logic.h
+++ src/Logic.h
@@ -8,7 +8,7 @@ class LObject {
     public:
         LObject ();
         glm::vec3 pos;
-        glm::vec2 rot;
+        glm::vec2 rot; //left right ; up down  |  (0 is left, 360 is toward right) ; (-90 is up, 90 is down)
         std::function<void()> onTick = [](){};
 };
 #endif

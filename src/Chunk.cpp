e071b703d939d602c62ea6046e41f20ab9ed9e1d
Nicholas Wolf <niko.m.wolf@gmail.com>
2025-04-02 12:49:48 -0600

    Changed so Y are height

--- src/Chunk.cpp
+++ src/Chunk.cpp
@@ -22,22 +22,22 @@ Chunk Chunk::genChunk (int cx, int cy) {
 	
 		// Generates terrain
 		for (int x = 0; x < 16; x++) {
-			for (int y = 0; y < 16; y++) {
+			for (int z = 0; z < 16; y++) {
 				// Get height value and scale it to terrain height
-				float heightValue = noise.GetNoise((float)(x + cx*16), (float)(y + cy*16));
+				float heightValue = noise.GetNoise((float)(x + cx*16), (float)(z + cz*16));
 				int terrainHeight = (int)((heightValue + 1) * 0.5 * (64 / 2)) + (64 / 4);
-				for (int z = 0; z < 64; z++) {
+				for (int y = 0; y < 64; y++) {
 					// Assign blocks based on Z level
-					if (z > terrainHeight) {
+					if (y > terrainHeight) {
 						c.blocks[x][y][z] = 0;  
 					} 
-					else if (z == terrainHeight) {
+					else if (y == terrainHeight) {
 						c.blocks[x][y][z] = 1;  
 					} 
-					else if (z > terrainHeight - 1) {
+					else if (y > terrainHeight - 1) {
 						c.blocks[x][y][z] = 2;  
 					} 
-					else if (z > 20) {
+					else if (y > 20) {
 						c.blocks[x][y][z] = 3;  
 					} 
 					else {
--- src/Chunk.h
+++ src/Chunk.h
@@ -12,9 +12,9 @@ class Chunk {
 
         glm::vec2 pos;
 
-        static int chunkCoordToWorld (int cx, int cy, float x, float y, float z); //chunk x and y, and chunk coords x y z
+        static int chunkCoordToWorld (int cx, int cz, float x, float y, float z); //chunk x and z, and chunk coords x y z
 
-        int blocks[16][16][64]; //xyz
+        int blocks[16][64][16]; //xyz
 
         static Chunk genChunk (int cx, int cy);
 

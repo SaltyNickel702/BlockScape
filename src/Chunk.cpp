13876dbc815277f2afc6be81345419b97bbbb725
Sean G <seangand9606@graniteschools.org>
2025-04-02 11:52:33 -0600

    Update Chunk.cpp

--- src/Chunk.cpp
+++ src/Chunk.cpp
@@ -5,16 +5,21 @@ Chunk Chunk::genChunk (int cx, int cy) {
 	Chunk c;
 	c.pos = glm::vec2(cx,cy);
 
-	//VVV do perlin noise stuff here VVV
-
 	int seed = 495804; // Change this int to change the seed
 	float x,y,z;
 
 	FastNoiseLite noise;  // Create noise generator
+	//Fast Noise settings
     noise.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2); 
 	noise.SetFrequency(0.01f);
     float noiseValue = noise.GetNoise(x, y,z);
-	noise.SetSeed(seed);  
+	noise.SetSeed(seed); 
+	noise.SetFractalType(FastNoiseLite::FractalType_FBm);
+	noise.SetFractalOctaves(3);
+	noise.SetFractalLacunarity(2.76f); 
+ 
+
+ 
 	
 	for (int x = 0; x < 16; x++) {
 		for (int y = 0; y < 16; y++) {

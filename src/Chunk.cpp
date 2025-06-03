0aadf69bb448423dfacae69489054e967e5ea81c
Nicholas Wolf <niko.m.wolf@gmail.com>
2025-06-03 17:12:58 -0600

    fixed another bug so I can show family

--- src/Chunk.cpp
+++ src/Chunk.cpp
@@ -1,7 +1,7 @@
 #include <FastNoise/FastNoiseLite.h>
 #include "Chunk.h"
 #include "World.h"
-#include "Multiplayer.h"
+// #include "Multiplayer.h"
 
 Chunk::Chunk () : loaded(false), modified(false), blocksLoaded(false), meshReady(false) {
 	mesh = new Model();
